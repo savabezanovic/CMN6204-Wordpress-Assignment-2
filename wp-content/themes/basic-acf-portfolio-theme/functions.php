d4062b75ea3f4a8e608de9000993adb0d6f7a89e
savabezanovic <savabezanovic@hotmail.com>
2017-10-14 12:38:45 +0200

    Cas footer scripts

--- wp-content/themes/basic-acf-portfolio-theme/functions.php
+++ wp-content/themes/basic-acf-portfolio-theme/functions.php
@@ -27,21 +27,45 @@ add_action('wp_enqueue_scripts', 'custom_scripts');
 function custom_scripts() {
 	wp_register_style(
 		'bootstrap',
-		'/wp-content/themes/basic-acf-portfolio-theme/vendor/bootstrap/css/bootstrap.min.css',
+		get_template_directory_uri().'/vendor/bootstrap/css/bootstrap.min.css',
 		[],
 		1.0,
 		false
 	);
 	wp_register_style(
 		'modern-business',
-		'/wp-content/themes/basic-acf-portfolio-theme/css/modern-business.css',
+		get_template_directory_uri().'/css/modern-business.css',
 		['bootstrap'],
 		1.0,
 		false
 	);
+	wp_register_script(
+		'jQuery',
+		get_template_directory_uri().'/vendor/jquery/jquery.js',
+		[],
+		3.21,
+		false
+	);
+	wp_register_script(
+		'popper',
+		get_template_directory_uri().'/vendor/popper/popper.min.js',
+		[],
+		1.111,
+		true
+	);
+		wp_register_script(
+		'bootstrap',
+		get_template_directory_uri().'/vendor/bootstrap/js/bootstrap.min.js',
+		[],
+		4.0,
+		true
+	);
 
 	wp_enqueue_style('bootstrap');
 	wp_enqueue_style('modern-business');
+	wp_enqueue_script('jQuery');
+	wp_enqueue_script('popper');
+	wp_enqueue_script('bootstrap');
 }
 
 ?>
